f21c6ec9f93cffbd43c9cdac36e9b733b210ec14
edem8 <eddieblack159@gmail.com>
2024-08-31 19:34:24 +0000

    feat: auth screens

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/outline";
+import { FcGoogle } from "react-icons/fc";
+
+const SignIn: FC = () => {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleGoogleSignIn = () => {};
+
+  const handleEmailSignIn = () => {
+    router.push("/dashboard");
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-sm p-8 space-y-6 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-center text-gray-100">
+          Sign In
+        </h2>
+
+        <button
+          className="w-full px-4 py-2 flex items-center justify-center border border-gray-500 rounded-lg hover:bg-gray-700"
+          onClick={handleGoogleSignIn}
+        >
+          <FcGoogle className="w-5 h-5 mr-2" />
+          Sign in with Google
+        </button>
+
+        <div>
+          <div className="mb-4">
+            <label className="block mb-2 text-gray-600">Email</label>
+            <input
+              type="email"
+              placeholder="myname@gmail.com"
+              className="w-full px-3 py-3 bg-[#020815] border border-gray-600 rounded-lg text-gray-200"
+            />
+          </div>
+
+          <div className="mb-4 relative">
+            <label className="block mb-2 text-gray-600">Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              className="w-full px-3 py-3 bg-[#020815] border border-gray-600 rounded-lg text-gray-100 pr-10"
+            />
+            <div
+              className="absolute inset-y-0 right-0 top-6 flex items-center px-3 text-gray-400 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <EyeOffIcon className="h-5 w-5" />
+              ) : (
+                <EyeIcon className="h-5 w-5" />
+              )}
+            </div>
+          </div>
+
+          <div className="text-right mb-6">
+            <a
+              href="/forgot-password"
+              className="text-sm text-indigo-400 hover:underline"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+
+        <button
+          className="w-full px-4 py-3 bg-indigo-600 text-white rounded-lg hover:bg-indigo-500"
+          onClick={handleEmailSignIn}
+        >
+          Sign In
+        </button>
+
+        <p className="mt-4 text-sm text-center text-gray-400">
+          Don’t have an account?{" "}
+          <a href="/signup" className="text-indigo-400 hover:underline">
+            Sign Up
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/outline";
+import { FcGoogle } from "react-icons/fc";
+
+const SignIn: FC = () => {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [agreeToTerms, setAgreeToTerms] = useState(false);
+  const [agreeToNewsletter, setAgreeToNewsletter] = useState(false);
+
+  const handleGoogleSignIn = () => {};
+
+  const handleEmailSignIn = () => {
+    router.push("/dashboard");
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-sm p-8 space-y-6 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-center text-gray-100">
+          Sign Up
+        </h2>
+
+        <button
+          className="w-full px-4 py-2 flex items-center justify-center border border-gray-500 rounded-lg hover:bg-gray-700"
+          onClick={handleGoogleSignIn}
+        >
+          <FcGoogle className="w-5 h-5 mr-2" />
+          Sign up with Google
+        </button>
+
+        <div>
+          <div className="mb-4">
+            <label className="block mb-2 text-gray-600">Email</label>
+            <input
+              type="email"
+              placeholder="myname@gmail.com"
+              className="w-full px-3 py-3 bg-[#020815] border border-gray-600 rounded-lg text-gray-200"
+            />
+          </div>
+
+          <div className="mb-4 relative">
+            <label className="block mb-2 text-gray-600">Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              className="w-full px-3 py-3 bg-[#020815] border border-gray-600 rounded-lg text-gray-100 pr-10"
+            />
+            <div
+              className="absolute inset-y-0 right-0 top-6 flex items-center px-3 text-gray-400 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <EyeOffIcon className="h-5 w-5" />
+              ) : (
+                <EyeIcon className="h-5 w-5" />
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-4">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="terms"
+              className="w-5 h-5 text-indigo-600 bg-[#020815] border-gray-600 rounded focus:ring-indigo-500"
+              checked={agreeToTerms}
+              onChange={() => setAgreeToTerms(!agreeToTerms)}
+            />
+            <label
+              htmlFor="terms"
+              className="ml-2 text-sm text-gray-400 cursor-pointer"
+            >
+              I agree to the{" "}
+              <a href="/terms" className="text-indigo-400 hover:underline">
+                Terms and Conditions
+              </a>{" "}
+              and{" "}
+              <a href="/privacy" className="text-indigo-400 hover:underline">
+                Privacy Policy
+              </a>
+            </label>
+          </div>
+
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="newsletter"
+              className="w-4 h-4 text-indigo-600 bg-[#020815] border-gray-600 rounded focus:ring-indigo-500"
+              checked={agreeToNewsletter}
+              onChange={() => setAgreeToNewsletter(!agreeToNewsletter)}
+            />
+            <label
+              htmlFor="newsletter"
+              className="ml-2 text-sm text-gray-400 cursor-pointer"
+            >
+              I agree to receive newsletters and updates.
+            </label>
+          </div>
+        </div>
+
+        <button
+          className="w-full px-4 py-3 bg-indigo-600 text-white rounded-lg hover:bg-indigo-500 mt-6"
+          onClick={handleEmailSignIn}
+          disabled={!agreeToTerms}
+        >
+          Sign Up
+        </button>
+
+        <p className="mt-4 text-sm text-center text-gray-400">
+          Already have an account?{" "}
+          <a href="/signin" className="text-indigo-400 hover:underline">
+            Sign In
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
