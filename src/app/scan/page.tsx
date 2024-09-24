9127d6f03c0d879b054bbc8dbf6b54cc19a3cf74
edem8 <eddieblack159@gmail.com>
2024-09-24 06:40:21 +0000

    chore: disable cors on-fetch

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -19,8 +19,7 @@ interface UploadResult {
   corrected: string[];
 }
 
-const UploadPage: React.FC = () => {
-  const [style, setStyle] = useState<string>("APA 7th edition");
+const Scan: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
   const router = useRouter();
   const [showLoader, setShowLoader] = useState<true | false>(true);
@@ -33,10 +32,6 @@ const UploadPage: React.FC = () => {
       setShowLoader(false);
     }
   }, [loading, user, router]);
-
-  const handleStyleChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
-    setStyle(e.target.value);
-  };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const selectedFile = e.target.files?.[0];
@@ -58,12 +53,11 @@ const UploadPage: React.FC = () => {
     }
 
     const formData = new FormData();
-    formData.append("style", style);
+    formData.append("style", "scan");
     formData.append("file", file);
 
     const uploadPromise = fetch(process.env.NEXT_PUBLIC_AI_MICROSERVICE || "", {
       method: "POST",
-      mode: "no-cors",
       body: formData,
     })
       .then((response) => response.json())
@@ -106,34 +100,16 @@ const UploadPage: React.FC = () => {
     <main className="relative flex min-h-screen flex-col items-center justify-center p-4 sm:p-6 lg:p-8 ">
       <div className="w-full max-w-md px-4 py-6 shadow-md rounded-lg ">
         <h1 className="text-xl md:text-2xl font-bold mb-6 text-white">
-          Upload Document
+          Scan Document
         </h1>
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-[#1a1a2e] before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-[#1a1a2e] after:via-[#0141ff6c] after:blur-2xl after:content-[''] before:dark:opacity-10 after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]"></div>
         <form onSubmit={handleSubmit} className="space-y-6 md:space-y-7">
           <div>
             <label
-              htmlFor="style"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Citation Style
-            </label>
-            <select
-              id="style"
-              value={style}
-              onChange={handleStyleChange}
-              className="mt-2 px-3 block w-full bg-gray-800 text-white border border-gray-600 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 h-12"
-            >
-              <option value="APA 7th edition">APA 7th edition</option>
-              <option value="MLA 9th edition">MLA 9th edition</option>
-            </select>
-          </div>
-
-          <div>
-            <label
               htmlFor="file"
               className="block text-sm font-medium text-gray-300"
             >
-              Select Doc
+              Document
             </label>
             <input
               id="file"
@@ -148,7 +124,7 @@ const UploadPage: React.FC = () => {
             type="submit"
             className="w-full py-3 px-4 bg-blue-800 text-white rounded-lg font-bold cursor-pointer hover:bg-blue-600 mt-6"
           >
-            Upload
+            Scan
           </button>
         </form>
       </div>
@@ -156,4 +132,4 @@ const UploadPage: React.FC = () => {
   );
 };
 
-export default UploadPage;
+export default Scan;
